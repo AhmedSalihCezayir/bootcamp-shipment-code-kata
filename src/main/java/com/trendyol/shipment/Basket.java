aab1a49744f91c412d0949d15cb40a216c0bf36b
Ahmed Salih Cezayir <asalihcezayir@gmail.com>
2023-09-11 09:32:42 +0300

    Initial, unclean working solution

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,16 @@
 package com.trendyol.shipment;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Basket {
 
     private List<Product> products;
+    private final Integer THRESHOLD = 3;
 
     public ShipmentSize getShipmentSize() {
-        return null;
+            return getRepeatingProductSize();
     }
 
     public List<Product> getProducts() {
@@ -16,5 +19,43 @@ public class Basket {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    private HashMap<ShipmentSize, Integer> getFrequencyMapOfProductsList() {
+        HashMap<ShipmentSize, Integer> frequencyMap = new HashMap<>();
+
+        for (Product product: products) {
+            ShipmentSize productSize = product.getSize();
+            if (frequencyMap.containsKey(productSize)) {
+                frequencyMap.put(productSize, frequencyMap.get(productSize) + 1);
+            } else {
+                frequencyMap.put(productSize, 1);
+            }
+        }
+
+        return frequencyMap;
+    }
+
+    private ShipmentSize getBiggestProductSize() {
+        ShipmentSize max = ShipmentSize.SMALL;
+
+        for (Product product: products) {
+            if (max.compareTo(product.getSize()) < 0) {
+                max = product.getSize();
+            }
+        }
+
+        return max;
+    }
+    
+    private ShipmentSize getRepeatingProductSize() {
+        HashMap<ShipmentSize, Integer> frequencyMap = getFrequencyMapOfProductsList();
+
+        for (Map.Entry<ShipmentSize, Integer> sizeFrequency: frequencyMap.entrySet()) {
+            if (sizeFrequency.getValue() >= THRESHOLD) {
+                return sizeFrequency.getKey().getNextBiggerSize();
+            }
+        }
+        return getBiggestProductSize();
     }
 }
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -5,5 +5,19 @@ public enum ShipmentSize {
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+
+    public ShipmentSize getNextBiggerSize() {
+        switch (this) {
+            case SMALL:
+                return MEDIUM;
+            case MEDIUM:
+                return LARGE;
+            case LARGE:
+            case X_LARGE:
+                return X_LARGE;
+            default:
+                throw new IllegalArgumentException("Invalid ShipmentSize: " + this);
+        }
+    }
 }
